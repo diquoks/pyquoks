3ffcc0656fafd72a5c4133dd32f86a1fea4cc27e
Denis Titovets <den232titovets@yandex.ru>
2025-12-05 20:18:18 +0300

    Улучшение скриптов для сборки проекта

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,3 @@
-# shellcheck disable=SC2046
-poetry add $(cat "../requirements.txt")
-
 rm -rf ../dist
 
 poetry build
---
+++ build/update_dependencies.sh
@@ -0,0 +1,2 @@
+# shellcheck disable=SC2046
+poetry add $(cat "../requirements.txt")
